dad7d8fcf3b903d65498721ed9d613741b53d2d7
Your Name <dangivishal624@gmail.com>
2025-09-28 09:48:32 +0530

    make a mango quiz

---
+++ info.py
@@ -0,0 +1,11 @@
+print("What are mangoes?")
+print("Mangoes are tropical stone fruits belonging to" \
+"the genus Mangifera.")
+print("They are native to South Asia but are now" \
+"grown in many tropical and subtropical regions worldwide.")
+quiz=input("Do you like mangoes? (yes/no): ")
+if quiz.lower() == "yes":
+    print("Great! Mangoes are delicious and nutritious.")
+else:
+    print("No worries! Everyone has different tastes.")
+
